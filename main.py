a6a292325b0c3fb95b7bf55b46d181cdc414bf80
Joshua Domantay <dom.josh2701@gmail.com>
2021-05-06 22:21:10 -0700

    Create main. Add convert and getDigits functions

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 # Decimal to Mayan
 # By Joshua Anthony Domantay
 # 6 May 2021
+
+def getDigits(n, arr):
+    pass
+
+def convert(n):
+    return "x"
+
+digit = input("Enter decimal digit: ")
+try:
+    digit = int(digit)
+    if(digit > 0):
+        x = convert(digit)
+        print(x)
+    else:
+        print("Digit must be greater than 0")
+except:
+    print("ERROR: Input cannot be converted to integer")

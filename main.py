6f46462c6c861bb7f5e2d20b13b5e4e76703c002
Joshua Domantay <dom.josh2701@gmail.com>
2021-05-06 22:35:40 -0700

    Finish printMid function

--- main.py
+++ main.py
@@ -8,9 +8,37 @@ def getDigits(n, arr):
 def printTopBottom(arr):
     for i in range(len(arr)):
         print("+----", end="")
-    print("+\n")
+    print("+")
 
 def printMid(arr):
+    x = 15
+    for i in range(4):
+        print("|", end="")
+        for j in range(len(arr)):
+            y = arr[j] - x
+            if(y == 5):
+                print("----", end="")
+                arr[j] -= 5
+            elif(y == 4):
+                print("....", end="")
+                arr[j] -= 4
+            elif(y == 3):
+                print("... ", end="")
+                arr[j] -= 3
+            elif(y == 2):
+                print(" .. ", end="")
+                arr[j] -= 2
+            elif(y == 1):
+                print(" .  ", end="")
+                arr[j] -= 1
+            else:
+                if(i != 3):
+                    print("    ", end="")
+                else:
+                    print(" @  ", end="")
+            print("|", end="")
+        print()
+        x -= 5
     pass
 
 def convert(n):
